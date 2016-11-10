c952912d2cae66845467487bedd272446b8a10ce
gillianomenezes <gillianomenezes@gmail.com>
2016-11-10 16:54:28 -0300

    Partial resolution of exercise 1

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+
+            var customers = new List<Customer>
+            {
+                new Customer() { Name = "customer 1" },
+                new Customer() { Name = "customer 2"}
+            };
+
+            CustomersViewModel customersViewModel = new CustomersViewModel()
+            {
+                Customers = customers
+            };
+
+            return View(customersViewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            
+            return 
+        }
+    }
+}
