021a6e77a1d5f911d2ca0428ffa876b78bb948c7
gilliano <gillianomenezes@gmail.com>
2016-11-21 21:46:55 -0300

    Revert "Add Genre model and migration"
    This reverts commit 8d1cb6e3455e8c0862e2f2feadbf434726dfd3a1.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using System.Data.Entity;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer != null)
+            {
+                return View(customer);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            var customers = _context.Customers;
+
+            return customers;
+        }
+    }
+}
