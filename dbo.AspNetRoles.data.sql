669103c81a055bf781f441348c64dcf5093fe860
Gilliano <Gilliano@LAPTOP-LS2EOVQ1>
2017-01-04 17:12:30 -0300

    Add new Role

---
+++ dbo.AspNetRoles.data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'2bfc791c-83eb-4829-a94f-ee7e26262b8a', N'CanManageMovies')
---
+++ dbo.AspNetUserRoles.data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'540d429b-79d1-4c05-aa64-3ab911d99dd0', N'2bfc791c-83eb-4829-a94f-ee7e26262b8a')
