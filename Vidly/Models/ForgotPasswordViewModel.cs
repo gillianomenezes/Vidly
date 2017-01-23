8b2ea08ec5a7b71c33fc61a2c595a5448332bdec
Gilliano <Gilliano@LAPTOP-LS2EOVQ1>
2017-01-23 16:29:22 -0300

    Bugs correction

---
+++ Vidly/Models/ForgotPasswordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
